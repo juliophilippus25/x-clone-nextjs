c3cc54d00fc0ddaa1dad9fb218073942c905da13
Julio Philippus <juliogerald25@gmail.com>
2024-10-13 18:06:38 +0700

    feat: improve component WhoToFollow

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,19 +1,25 @@
+import Link from "next/link";
 import Image from "next/image";
 import Button from "./Buttons";
 import { MdVerified } from "react-icons/md";
 
-export default function Avatar() {
+interface AvatarProps {
+    name: string;
+    username: string;
+}
+
+export default function Avatar({ username, name }: AvatarProps) {
     return (
         <div className="flex items-center justify-between py-2 rounded-full w-full cursor-pointer">
-            <div className="flex items-center">
+            <Link href={`/${username}`} className="flex items-center">
                 <Image src="/avatar.jpg" alt="Avatar" width={40} height={40} className="rounded-full w-10 h-10" />
                 <div className="ml-2 flex flex-col">
                     <p className="text-white hidden lg:flex items-center">
-                        Julio Philippus <MdVerified className="ml-1 text-lg text-customColor" />
+                        {name} <MdVerified className="ml-1 text-lg text-customColor" />
                     </p>
-                    <p className="text-gray-600 hidden lg:block">@julzybaee</p>
+                    <p className="text-gray-600 hidden lg:block">@{username}</p>
                 </div>
-            </div>
+            </Link>
             <div className="ml-auto">
                 <Button
                     bgColor="bg-white"
--- src/app/components/home/WhoToFollow.tsx
+++ src/app/components/home/WhoToFollow.tsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
 import Avatar from "../Avatar";
+import { getUsersFromLocalStorage } from '@/app/libs/data';
 
 export default function WhoToFollow() {
-    return (
-        <>
-            <div className="border border-gray-800 p-4 rounded-lg mt-4">
-                <h2 className="text-lg font-bold">Who to follow</h2>
-                <Avatar />
-                <Avatar />
-                <Avatar />
+    // Retrieve users from local storage
+    const users = getUsersFromLocalStorage();
 
-                <Link href="#" className="text-customColor">
-                    Show more
-                </Link>
-            </div>
-        </>
+    return (
+        <div className="border border-gray-800 p-4 rounded-lg mt-4">
+            <h2 className="text-lg font-bold">Who to follow</h2>
+            {users.length > 0 ? (
+                users.map(user => (
+                    <Avatar key={user.username} username={user.username}
+                        name={user.name} />
+                ))
+            ) : (
+                <p className="text-gray-500">No users to follow</p>
+            )}
+
+            <Link href="#" className="text-customColor">
+                Show more
+            </Link>
+        </div>
     );
 }
